06fd85b1401005c51f190c02d62f745b8953b82c
reZach <reZach@users.noreply.github.com>
2024-05-11 00:00:10 -0500

    small copy update

--- src/splashscreen.js
+++ src/splashscreen.js
@@ -13,7 +13,7 @@ function Splashscreen() {
         <div className="App text-white">
             <Header page={"start"}/>
             <h1 className="display-1 bold">[working title]</h1>
-            <h4 style={{marginBottom:"80px"}}>A project meant to inspire you by showing that even one small act of love<br /> can have rippling effects across the world.</h4>
+            <h4 style={{marginBottom:"80px"}}>A project meant to inspire you by showing that even one small act of love<br /> can have a rippling effect across the world.</h4>
             <h1>How does it work?</h1>
             <p>
                 If you have landed on this page, you have been given a coin.<br /> Your task is to do something you would not normally do, to love or care for another person.<br /> Once you have done this, give them the coin and tell them do to the same.<br /> Log <em>where</em> you did this act of service on <a href="/input">this page</a>. You can view how far the coin has traveled on <a href="/coinpass">this page</a>.
