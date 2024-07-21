543d0e973b82f35cc58d53fccbe1451c8874f3fb
reZach <reZach@users.noreply.github.com>
2024-07-20 23:07:34 -0500

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,10 +22,10 @@ function Home() {
                         <h1 className="display-3 header">How does it work<span style={{ position: "relative", top: "-8px" }}>?</span></h1>
                         <p>If you are visiting this page and have been given a coin, please follow the steps below.</p>
                         <ol style={{ listStyleType: "decimal" }}>
-                            <li style={{ textAlign: "left" }}>Write down the code (which begins with two letters) on your coin on a piece of spare paper.</li>
+                            <li style={{ textAlign: "left" }}>Write down the code (which begins with two letters), and number (underneath the code), on your coin on a piece of spare paper.</li>
                             <li style={{ textAlign: "left" }}>Do something you would not normally do; to love or care for another person. If you need ideas of what you can do, check out some examples on our <a href="/#faq">faq page</a>.</li>
                             <li style={{ textAlign: "left" }}>Once you have done something for someone else, give this person your coin and tell them to do something for someone else.</li>
-                            <li style={{ textAlign: "left" }}>Using the code you wrote down in step 1, log <em>where</em> you did this act of service on <a href="/#action">this page</a>. You can view how far the coin has traveled on <a href="/#map">this page</a>.</li>
+                            <li style={{ textAlign: "left" }}>Using the code you wrote down in step 1, log <em>where</em> you did this act of service on <a href="/#action">this page</a>. You can view how far your coin has traveled on <a href="/#map">this page</a> using the number found on your coin.</li>
                         </ol>
                         <p><em>(You can view the <a href="/#faq">faq page</a> to check how soon your action will be updated on the map)</em></p>
                         <div className="cta-buttons">
